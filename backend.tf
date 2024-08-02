5bb494f9d53a04c4254a8fed73010108ef505212
sterlingvin <135068864+sterlingvin@users.noreply.github.com>
2024-08-02 19:42:55 -0400

    location changed

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "backend-bucket007"
     key = "main"
-    region = "us-east-1"
+    region = "ca-central-1"
     dynamodb_table = "my-dynamo-db-table-tf"
   }
 }
